b0187f424275f6bb10afadd0fe733ecb43b50e3a
Shizuna Ito <adfa1728@chiba-u.jp>
2017-06-22 09:28:21 +0000

    Fooコントローラを生成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  root 'application#hello'
+
+  get 'foo/bar'
+  get 'foo/baz'
   get 'static_pages/home'
-
   get 'static_pages/help'
-
-    root 'application#hello'
 end
