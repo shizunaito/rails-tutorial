6e74b28aa32d7f0668742b7e3818062fbc729fc4
shizuna.ito <shizuna.ito@pepabo.com>
2017-06-29 16:05:40 +0900

    emailのデータベース上のバリデーションを追加

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -55,4 +55,11 @@ class UserTest < ActiveSupport::TestCase
     assert_not duplicate_user.valid?
   end
 
+  test "email address should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.Com"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
 end
