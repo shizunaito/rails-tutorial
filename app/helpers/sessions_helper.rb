b0b53ea81be1921c1fd64e8424392a443573c94a
shizuna.ito <shizuna.ito@pepabo.com>
2017-07-04 14:12:47 +0900

    SessionsヘルパーでCookiesを用いたログインを追加

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,17 @@
 module SessionsHelper
+
+  # 渡されたユーザーでログインする
+  def log_in(user)
+    session[:uesr_id] = user.id
+  end
+
+  # 現在ログイン中のユーザーを返す（いる場合）
+  def current_user
+    @current_user ||= User.find_by(id: session[:uesr_id])
+  end
+
+  # ユーザーがログインしていればtrue、その他ならfalseを返す
+  def logged_in?
+    !current_user.nil?
+  end
 end
