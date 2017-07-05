39c2c216b6717366785b1a02bd24a0764636707c
shizuna.ito <shizuna.ito@pepabo.com>
2017-07-05 16:37:24 +0900

    ユーザーIDと記憶トークンの永続cookieを作成し、ログイン状態を保持

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,6 +26,7 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, User.digest(remember_token))
   end
 
+  # 渡されたトークンがダイジェストと一致したらtrueを返す
   def authenticated?(remember_token)
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
