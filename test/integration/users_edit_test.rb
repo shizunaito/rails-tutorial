207de2dfef95283cd8a519f69da4b15c690e264c
shizuna.ito <shizuna.ito@pepabo.com>
2017-07-07 16:37:00 +0900

    ユーザーがログインした後その編集ページにリダイレクトされるよう修正し、そのテストを追加

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -18,10 +18,12 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_select "div.alert.alert-danger", "The form contains 4 errors."
   end
 
-  test "successuful edit" do
-    log_in_as(@user)
+  test "successuful edit with friendly forwarding" do
     get edit_user_path(@user)
-    assert_template 'users/edit'
+    assert_equal session[:forwarding_url], edit_user_url(@user)
+    log_in_as(@user)
+    assert session[:forwarding_url].nil?
+    assert_redirected_to edit_user_url(@user)
     name = "Foo Bar"
     email = "foo@bar.com"
     patch user_path(@user), params: { user: { name: name,
