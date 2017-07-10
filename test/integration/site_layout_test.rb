b9f797df7cd628421a939de7e31e740342fce6c7
shizuna.ito <shizuna.ito@pepabo.com>
2017-07-10 11:58:26 +0900

    演習：リンクのテスト（主にログイン時）を追加

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,11 +2,17 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
-  test "layout links" do
+  def setup
+    @user = users(:michael)
+  end
+
+  test "layout links without login" do
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count:2
     assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", signup_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
 
@@ -15,5 +21,19 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
 
     get signup_path
     assert_select "title", full_title("Sign up")
+
+    get login_path
+    assert_select "a[href=?]", signup_path
+  end
+
+  test "layout links with login" do
+    log_in_as(@user)
+    get root_path
+    assert_select "a[href=?]", root_path, count:2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
+    assert_select "a[href=?]", logout_path
   end
 end
