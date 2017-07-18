d2eeb5027f5342188d1237d1302ddfc0a3388a72
shizuna.ito <shizuna.ito@pepabo.com>
2017-07-18 18:53:27 +0900

    スペースが抜けていた部分を修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
-  attr_accessor :remember_token,:activation_token, :reset_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
   before_create :create_activation_digest
   validates :name, presence: true, length: { maximum: 50 }
