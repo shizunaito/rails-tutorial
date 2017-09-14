f6c94b7f9316c5e42e2162a1f4cadc475db1141c
Shohei Ogata <shohei.ogata@pepabo.com>
2017-09-14 15:33:35 +0900

    デプロイのためのファイルを追加

---
+++ config/deploy.rb
@@ -0,0 +1,25 @@
+# config valid only for current version of Capistrano
+lock "3.9.0"
+
+set :application, "sample_app"
+set :repo_url, "https://github.com/shizunaito/rails-tutorial.git"
+set :branch, "master"
+set :rails_env, 'production'
+
+task :start do
+  on roles(:web) do
+    execute "sudo systemctl start puma"
+  end
+end
+
+task :stop do
+  on roles(:web) do
+    execute "sudo systemctl stop puma"
+  end
+end
+
+task :restart do
+  on roles(:web) do
+    execute "sudo systemctl restart puma"
+  end
+end
