37cdd1f13a07a7590dbe571716560f6b4c18c021
Shohei Ogata <shohei.ogata@pepabo.com>
2017-10-02 18:44:07 +0900

    ランダム取得処理をモデルに写した

--- app/controllers/random_feeds_controller.rb
+++ app/controllers/random_feeds_controller.rb
@@ -4,19 +4,12 @@ class RandomFeedsController < ApplicationController
   def show
     @microposts = []
     while @microposts.length < 10 do
-      @microposts.concat(get_rand_microposts(10))
+      @microposts.concat(Micropost.get_rand_microposts(10))
     end
     @microposts = @microposts[0..9]
   end
   
   private
-
-    def get_rand_microposts(getFeedLength)
-      count = Micropost.count() - getFeedLength
-      raise Errors::InvalidFeedLengthError if count < 0
-      randIndex = Random.rand(0..count)
-      Micropost.where("id between #{randIndex} and #{randIndex + getFeedLength}")
-    end
 
     def handle_error(error)
       render json: {message: error.detail}, status: error.status_code
